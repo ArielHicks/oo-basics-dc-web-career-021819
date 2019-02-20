13079b2cb2a47998e7c5db117963c202574f5c31
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-20 22:11:49 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,9 +3,10 @@ class Book
 
   attr_accessor :title
 
-def initialize(title)
-  @title = title
-end
+  def initialize(title)
+    @title = title
+  end
+
   def turn_page
     puts "Turning the page...wow, you're a fast reader!"
   end
