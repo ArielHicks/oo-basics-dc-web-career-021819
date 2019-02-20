7ca1a1c7ba1c7e04d528f2909692ecddc902c22a
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-20 22:10:29 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
 # Make your shoe class here!
+class Shoe
+
+attr_accessor :color, :size, :material
+
+    def initialize(brand)
+      @brand = brand
+
+    end
+
+    def brand
+      @brand
+    end
+
+end
