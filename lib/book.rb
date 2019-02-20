a91177d73a0e1c1277e57a3087129cb6d698c23f
Ariel Hicks <hicks.ariel@gmail.com>
2019-02-20 22:11:22 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,10 +1,10 @@
 
 class Book
 
-attr_accessor :title
+  attr_accessor :title
 
-def turn_page
-  puts "Turning the page...wow, you're a fast reader!"
-end
+  def turn_page
+    puts "Turning the page...wow, you're a fast reader!"
+  end
 
 end
